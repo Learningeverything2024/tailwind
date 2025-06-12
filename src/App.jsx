eb98402c62ddf574882d0a99052428814f6a2ad0
Shivam Bansal <shivambansal@Shivams-MacBook-Air.local>
2025-06-12 15:45:32 +0530

    basic changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/header"
-import Body from "./components/body"
-import Footer from "./components/footer"
+import Header from "./components/Header";
+import Body from "./components/Body"
+import Footer from "./components/Footer"
 
 function App() {
 
